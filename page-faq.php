bc83b24033e4b8e23dd1584b8de9442922629446
Julia Bode <julia.bode@hmmh.de>
2014-05-19 12:20:11 +0200

    Add faq template page.

---
+++ page-faq.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: FAQ
+*/
+?>
+
+<div class="large-12 medium-12 small-12 column m-t-xxl m-b-xxl">
+    <?php get_template_part('templates/page', 'header'); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+
+    <div class="accordion vertical" id="accordion">
+
+        <?php if( have_rows('faq_repeater') ) { ?>
+        <ul>
+            <?php while ( have_rows('faq_repeater') ) { the_row(); ?>
+                <li>
+                    <h2>
+                        <a href="#"><i class="fa fa-chevron-right"></i><?php the_sub_field('faq_question'); ?></a>
+                    </h2>
+                    <div class="acc-content">
+                        <?php the_sub_field('faq_answer'); ?>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+        <?php } ?>
+
+    </div>
+
+</div>
