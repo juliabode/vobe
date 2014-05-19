26453cc33c1c2ebae538e52c46ec5ab103e641fd
Julia Bode <julia.bode@hmmh.de>
2014-05-19 13:12:37 +0200

    Add new templates for different number of col.

---
+++ page-2_col.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: 2 Spalten
+*/
+?>
+
+<div class="large-12 medium-12 small-12 column m-t-xxl m-b-xxl">
+    <?php get_template_part('templates/page', 'header'); ?>
+
+    <ul class="large-block-grid-2 medium-block-grid-2 small-block-grid-1">
+        <li>
+            <?php get_template_part('templates/content', 'page'); ?>
+        </li>
+
+        <li>
+            <?php the_field('2-col-text_col2'); ?>
+        </li>
+    </ul>
+</div>
---
+++ page-3_col.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: 3 Spalten
+*/
+?>
+
+<div class="large-12 medium-12 small-12 column m-t-xxl m-b-xxl">
+  <?php get_template_part('templates/page', 'header'); ?>
+
+  <ul class="large-block-grid-3 medium-block-grid-3 small-block-grid-1">
+    <li>
+      <?php get_template_part('templates/content', 'page'); ?>
+    </li>
+
+    <li>
+      <?php the_field('3-col-text_col2'); ?>
+    </li>
+
+    <li>
+      <?php the_field('3-col-text_col3'); ?>
+    </li>
+  </ul>
+</div>
