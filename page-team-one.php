688d251a9638ed4f4f67deb7cfb6e244c5038aec
Julia Bode <julia.bode@hmmh.de>
2014-05-19 12:17:53 +0200

    Add member pages.

---
+++ page-team-one.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Template Name: Team Page One
+*/
+?>
+
+<div class="large-12 medium-12 small-12 column m-t-xxl m-b-xxl">
+    <?php get_template_part('templates/page', 'header'); ?>
+
+    <div class="m-b-xxxl">
+        <?php get_template_part('templates/content', 'page'); ?>
+    </div>
+
+    <?php
+        for ( $i = 1; $i <= 6; $i++ ) {
+            $memberName        = get_field('member_' . $i . '_name');
+            $memberImage       = get_field('member_' . $i . '_image');
+            $memberPosition    = get_field('member_' . $i . '_position');
+            $memberDescription = get_field('member_' . $i . '_description'); 
+
+            if ( !empty($memberName) ) {
+
+                if ( $i % 2 !== 0 ) { ?>
+
+                    <ul class="large-block-grid-2 medium-block-grid-2 small-block-grid-1 member-page-1__left">
+                        <li class="alignleft">
+                            <?php
+                            if ( !is_null($memberImage['sizes']['member-fotos']) ) : ?>
+                                <div class="member-page__image-container">
+                                    <img class="left" src="<?php echo $memberImage['sizes']['member-fotos']; ?>" alt="<?php echo $memberImage; ?>">
+                                </div>
+                            <?php endif ?>
+                        </li>
+
+                        <li class="member-page-1__infos">
+                            <h3><?php echo $memberName; ?></h3>
+                            <p class="cursive"><?php echo $memberPosition; ?></p>
+                            <div>
+                                <?php echo $memberDescription; ?>
+                            </div>
+                        </li>
+                    </ul>
+
+            <?php } else { ?>
+
+                    <ul class="large-block-grid-2 medium-block-grid-2 small-block-grid-1 member-page-1__right">
+                        <li class="member-page-1__infos">
+                            <h3><?php echo $memberName; ?></h3>
+                            <p class="cursive"><?php echo $memberPosition; ?></p>
+                            <div>
+                                <?php echo $memberDescription; ?>
+                            </div>
+                        </li>
+
+                        <li class="alignright">
+                            <?php
+                            if ( !is_null($memberImage['sizes']['member-fotos']) ) : ?>
+                                <div class="member-page__image-container">
+                                    <img class="right" src="<?php echo $memberImage['sizes']['member-fotos']; ?>" alt="<?php echo $memberImage; ?>">
+                                </div>
+                            <?php endif ?>
+                        </li>
+                    </ul>
+
+            <?php }
+            }
+        }
+    ?>
+
+</div>
---
+++ page-team-two.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: Team Page Two
+*/
+?>
+
+<div class="large-12 medium-12 small-12 column m-t-xxl m-b-xxl">
+    <?php get_template_part('templates/page', 'header'); ?>
+
+    <div class="m-b-xxxl">
+        <?php get_template_part('templates/content', 'page'); ?>
+    </div>
+
+    <ul class="large-block-grid-2 medium-block-grid-2 small-block-grid-1">
+    <?php
+        for ( $i = 1; $i <= 6; $i++ ) {
+            $memberName        = get_field('member_' . $i . '_name');
+            $memberImage       = get_field('member_' . $i . '_image');
+            $memberPosition    = get_field('member_' . $i . '_position');
+            $memberDescription = get_field('member_' . $i . '_description'); 
+
+            if ( !empty($memberName) ) { ?>
+
+                <li>
+                    <div class="member-page-2__wrapper pos--rel">
+                        <div class="member-page-2__image-wrapper pos--rel">
+                            <img src="<?php echo $memberImage['sizes']['member-fotos']; ?>" alt="<?php echo $memberImage; ?>">
+                            <h3 class="pos--abs"><?php echo $memberName; ?></h3>
+                        </div>
+                        <div class="member-page-2__infos pos--abs special-box">
+                            <p class="cursive"><?php echo $memberPosition; ?></p>
+                            <div>
+                                <?php echo $memberDescription; ?>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+
+            <?php }
+        }
+    ?>
+    </ul>
+
+</div>
