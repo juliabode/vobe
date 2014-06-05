f0fe3776513f73b38c7ae1c2792e49f04bd617b8
Julia Bode <julia.bode@hmmh.de>
2014-06-05 09:58:08 +0200

    Add partner page template.

---
+++ page-partner.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Partner
+*/
+?>
+
+<div class="large-12 medium-12 small-12 column">
+    <?php get_template_part('templates/page', 'header'); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+
+    <div class="partner-logos">
+        <?php if( have_rows('partner_logos') ) { ?>
+        <ul class="large-block-grid-3 medium-block-grid-3 small-block-grid-1">
+            <?php while ( have_rows('partner_logos') ) { the_row(); ?>
+                <li>
+                    <?php $sub = get_sub_field('logo_image'); ?>
+                    <img src="<?php echo $sub['url']; ?>">
+                </li>
+            <?php } ?>
+        </ul>
+        <?php } ?>
+    </div>
+
+</div>
