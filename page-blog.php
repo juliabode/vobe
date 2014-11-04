8d2ae6b7bea20b6c0705b5102a643cb343e0f9c0
Julia Bode <julia.bode@hmmh.de>
2014-11-04 12:19:05 +0100

    Add changes to news page.

--- page-blog.php
+++ page-blog.php
@@ -6,14 +6,18 @@ Template Name: Blog
 
 
 <div class="large-12 medium-12 small-12 column">
-    <?php get_template_part('templates/page', 'header'); ?>
-    <?php query_posts( 'post_type=post&orderby=date' ); ?>
+    <?php get_template_part('templates/page', 'header');
+          $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+          query_posts( 'post_type=post&orderby=date&posts_per_page=5&paged=' . $paged ); ?>
 
     <div class="row">
         <div class="small-9 medium-9 small-12 left">
             <?php
                 get_template_part('templates/content', 'single');
             ?>
+            <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
+            <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
         </div>
 
         <aside class="sidebar column small-3 medium-3 small-12 special-box" role="complementary">
